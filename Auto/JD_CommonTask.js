60a98618ae856647762d64168cae99ff2258dbb8
Cool_rui <56864168+Coolrui1816@users.noreply.github.com>
2021-09-25 17:11:38 +0800

    整体延长等待时间，以保证任务执行成功

--- Auto/JD_CommonTask.js
+++ Auto/JD_CommonTask.js
@@ -9,7 +9,7 @@ while (text("21f6d36e11451cdd").findOnce() == null) {
     sleep(5000);
 }
 console.show();//开启悬浮窗
-if(text("21f6d36e11451cdd").exists()){
+if(text("21f6d36e11451cdd").exists()){//可能需要切换场景，建议手动打开任务列表
     console.info("成功进入活动界面");
     click(712,2670)
     sleep(2000);
@@ -19,36 +19,36 @@ if(text("去完成").exists()){
     //关键字作为任务类型
     var hasTask = true;
     while (hasTask) {
-        if (textMatches(/.*[0-9]S.*/).exists() && textMatches(/.*[0-9]S.*/).findOnce().parent().child(8).text() == "去完成") {
+        if (textMatches(/.*[0-11]S.*/).exists() && textMatches(/.*[0-11]S.*/).findOnce().parent().child(8).text() == "去完成") {
             console.info("开始浏览任务");
-            textMatches(/.*[0-9]S.*/).findOnce().parent().child(8).click();
-            sleep(9000);
+            textMatches(/.*[0-11]S.*/).findOnce().parent().child(8).click();
+            sleep(11000);
             console.log("任务完成");
         } else if (textContains("加购").exists() && textContains("加购").findOnce().parent().child(8).text() == "去完成") {
             console.info("开始加购任务");
             textContains("加购").findOnce().parent().child(8).click();
-            sleep(3000);
+            sleep(4000);
             for (var i = 0; i < 5; i++) {
                 className("android.view.View").scrollable(true).depth(15).findOne().child(i).child(0).child(4).click();
                 console.log("加购第" + (i+1) + "个商品");
-                sleep(3000);
+                sleep(4000);
                 back();
-                sleep(3000);
+                sleep(4000);
             }
             console.log("浏览加购完成，返回");
         } else if (textContains("浏览").exists() && textContains("浏览").findOnce().parent().child(8).text() == "去完成") {
             console.info("开始浏览任务");
             textContains("浏览").findOnce().parent().child(8).click();
-            sleep(3000);
+            sleep(11000);
             console.log("浏览完成，返回");
         } else if (textStartsWith("成功关注").exists() && textStartsWith("成功关注").findOnce().parent().child(8).text() == "去完成") {
             console.info("开始关注任务");
             textStartsWith("成功关注").findOnce().parent().child(8).click();
-            sleep(3000);
+            sleep(4000);
             console.log("任务完成，返回");
         } else if (textContains("开通品牌会员").exists() && textContains("开通品牌会员").findOnce().parent().child(8).text() == "去完成") {
             textContains("开通品牌会员").findOnce().parent().child(8).click();
-            sleep(1000);//如果已经入会，只要点了去完成就行，缩短运行时间
+            sleep(2000);//如果已经入会，只要点了去完成就行，缩短运行时间
             if(textContains("加入店铺会员").exists()){
                 console.log("脚本结束（涉及个人隐私,请手动加入店铺会员或者忽略加入会员任务)");
                 break;
